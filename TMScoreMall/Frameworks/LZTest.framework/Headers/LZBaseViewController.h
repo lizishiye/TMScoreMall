6e780e37dba9d6935ad511d0acac59233c309860
luxiaoxin <779474462@qq.com>
2018-10-19 12:52:12 +0800

    first commit for version 0.0.1

---
+++ TMScoreMall/Frameworks/LZTest.framework/Headers/LZBaseViewController.h
@@ -0,0 +1,21 @@
+//
+//  LZBaseViewController.h
+//  TMPointMall
+//
+//  Created by Admin on 2018/10/12.
+//  Copyright © 2018年 Admin. All rights reserved.
+//
+
+#import <TMSDK/TMSDK.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZBaseViewController : TMViewController
+
+- (void) lz_setUpSubViews;
+
+- (void) changeNavBackIem;
+
+@end
+
+NS_ASSUME_NONNULL_END
