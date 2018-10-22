cca93d636797be9a8a9607084bcfc19f3e83b8c2
luxiaoxin <779474462@qq.com>
2018-10-22 20:36:37 +0800

    first commit for version 0.0.7.1

---
+++ TMScoreMall/Classes/TMScoreMall.framework/Headers/CustomProcess.h
@@ -0,0 +1,39 @@
+//
+//  CustomProcess.h
+//  SocialNetwork
+//
+//  Created by yanll on 16/10/13.
+//  Copyright © 2016年 ZYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define LZShowHud(String)  dispatch_async(dispatch_get_main_queue(), ^{\
+        [SVProgressHUD setBackgroundColor:LZ_ColorHex_Alpha(000000, 0.6)];\
+        [SVProgressHUD setForegroundColor:LZ_ColorHex(ffffff)];\
+        [SVProgressHUD showImage:nil status:String];\
+        [SVProgressHUD dismissWithDelay:1.0f];\
+});\
+
+#define LZHideProgress dispatch_async(dispatch_get_main_queue(), ^{[[CustomProcess share] dismiss];});
+
+#define LZShowProgress dispatch_async(dispatch_get_main_queue(), ^{[[CustomProcess share] show];});
+
+
+@interface CustomProcess : UIView
+
+@property (weak, nonatomic) IBOutlet UIView *giftBgView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *loadingImgView;
+
+@property (weak, nonatomic) IBOutlet UILabel *textLB;
+
+
++ (instancetype)share;
+- (void)show;
+
+- (void)dismiss;
+
+
+
+@end
---
+++ TMScoreMall/Classes/TMScoreMall.framework/Headers/LZConversionRecordViewController.h
@@ -0,0 +1,17 @@
+//
+//  LZConversionRecordViewController.h
+//  TMPointMall
+//
+//  Created by Admin on 2018/10/16.
+//  Copyright © 2018年 Admin. All rights reserved.
+//
+
+#import "LZBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZConversionRecordViewController : LZBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
