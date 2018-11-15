d15f807acefaffc98e199ee5113672056ef253ac
李亚林 <1063357883@qq.com>
2018-11-15 14:01:22 +0800

    first commit for version 0.0.7.50

--- TMScoreMall/Classes/TMScoreMall.framework/Headers/g01jfsc_zk65mLZHomeViewController.h
+++ TMScoreMall/Classes/TMScoreMall.framework/Headers/g01jfsc_zk65mLZHomeViewController.h
@@ -12,12 +12,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface g01jfsc_zk65mLZHomeViewController : g01jfsc_zk65mLZBaseViewController
 
-/*
- *
- *是否隐藏底部tabbar。默认NO。若是没有tabbar或者隐藏或者二级界面，请赋值YES。
- */
-@property (nonatomic, assign) BOOL isHiddenTabBar;
-
 @end
 
 NS_ASSUME_NONNULL_END
